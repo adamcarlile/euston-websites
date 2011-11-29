0c50eeafc845379ce4c258557f3545bbf3e47d0f
Lee Henson <lee.m.henson@gmail.com>
2011-11-29 14:47:54 +0000

    Only raise not_parseable callbacks from JSON.decode exceptions
    Without this change, any errors generated in the invalid or valid
    callbacks would also get swallowed by the rescue block, leading
    to erroneous not_parseable callbacks and much head-scratching.

--- lib/euston-websites/api_command_request_parsing/abstract_parser.rb
+++ lib/euston-websites/api_command_request_parsing/abstract_parser.rb
@@ -9,17 +9,23 @@ module ApiCommandRequestParsing
     end
 
     def parse
+      valid = true
+
       begin
         commands = [transform_hash_to_commands(ActiveSupport::JSON.decode(@body))].flatten
-        invalid_commands = commands.reject { |c| c.valid? }
-
-        if invalid_commands.any?
-          callback :invalid, invalid_commands
-        else
-          callback :valid, commands
-        end
       rescue StandardError => e
         callback :not_parseable
+        valid = false
+      end
+
+      return unless valid
+
+      invalid_commands = commands.reject { |c| c.valid? }
+
+      if invalid_commands.any?
+        callback :invalid, invalid_commands
+      else
+        callback :valid, commands
       end
     end
   end
